f354a777bcee858f47ea91df2b4467b1e1eabb26
The_Greatest <82040240+polar51@users.noreply.github.com>
2022-02-11 19:38:12 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,11 +1,30 @@
+var oreha = document.getElementsByClassName('oreha_con');
+var oreha_hard = document.getElementsByClassName('oreha_hard_con');
+
+var argos = document.getElementsByClassName('argos_con');
+
+var baltan = document.getElementsByClassName('baltan_con');
+var baltan_hard = document.getElementsByClassName('baltan_hard_con');
+
+var biakiss = document.getElementsByClassName('biakiss_con');
+var biakiss_hard = document.getElementsByClassName('biakiss_hard_con');
+
+var koukusaton = document.getElementsByClassName('koukusaton_con');
+
+var abrelshud = document.getElementsByClassName('abrelshud_con');
+var abrelshud_hard = document.getElementsByClassName('abrelshud_hard_con');
+
+
+
 
 // 캐릭터 인포
 
-var char1_level = document.getElementById('char_level');
-var char1_name = document.getElementById('char_name');
 
 
-document.getElementById('button1').onclick = function(){
+
+document.getElementById('button1').addEventListener('click', ()=>{
+  var char1_level = document.getElementById('char_level');
+  var char1_name = document.getElementById('char_name');
 
   var char1_info1 = document.createElement('div'); 
   char1_info1.textContent = char1_level.value;
@@ -18,67 +37,85 @@ document.getElementById('button1').onclick = function(){
 
   document.getElementById('char_info').appendChild(char1_info2); 
   char1_name.remove();
-}
+
+  //정보 저장 버튼 삭제?
+
+  var char1_week_raid = char1_level.value;
+  var i = 1;
+  if(char1_week_raid>=1325){
+    oreha.classList.add('show')
+  }
+
+},{once:true});
+
+
+
 
 
 
 //일일 컨텐츠
 
-var kaos_chk1 = document.getElementById('kaos_check_box1');
-var kaos_chk2 = document.getElementById('kaos_check_box2');
+// var kaos_chk1 = document.getElementById('kaos_check_box1');
+// var kaos_chk2 = document.getElementById('kaos_check_box2');
 
 
 var kaos_huge = Array.from(document.getElementsByClassName('kaos_huge'));
-
 var kaos_huge_on = Array.from(document.getElementsByClassName("kaos_huge on"));
-
-function kaos_checked(){
-  마지막 순서의 휴게 게이지의 색을 투명으로 한다 체크 해제시 마지막 순서의 휴게 게이지의 색을 채워준다
-
-}
+var kaos_huge_i = kaos_huge_on[kaos_huge_on.length-1];
 
 
-
-
-
-
-  // kaos_huge.forEach(function(kaos_huge,index){
-  //   if(kaos_huge.classList.contains('on')){
-  //     kaos_huge.classList.remove('on');
-  //     kaos_huge.classList.add('off');
-  //   } else {
-  //     kaos_huge.classList.remove('off');
-  //     kaos_huge.classList.add('on');
-  //   }
-  // })
-
-
-// kaos_huge.forEach(function(kaos_huge,index){
-
-//   document.getElementById('kaos_check_box1').onclick = function(){
-
-
-//     if(kaos_chk1.checked){
-//       kaos_huge.style.backgroundColor = 'transparent'
-//     } else{
-//       kaos_huge.style.backgroundColor = 'green'
-//     }
-// }
-
-// })
+function kaos_checked(checked){
+  var arr = Array.from(document.getElementsByClassName("kaos_huge on"));
+  var i = arr[arr.length-1];
+  var x = kaos_huge_on[arr.length];
+  if(checked.checked==true){
+    i.classList.remove('on')
+  } else {
+    x.classList.add('on')
+  }
+};
 
 
 
-// kaos_huge.forEach(function(kaos_huge,index){
+// var guardian_chk1 = document.getElementById('guardian_check_box1');
+// var guardian_chk2 = document.getElementById('guardian_check_box2');
 
-//   document.getElementById('kaos_check_box2').onclick = function(){
 
-//     if(kaos_chk2.checked){
-//       kaos_huge.style.backgroundColor = 'transparent'
-//     } else{
-//       kaos_huge.style.backgroundColor = 'green'
-//     }
-//     }
+var guardian_huge = Array.from(document.getElementsByClassName('guardian_huge'));
+var guardian_huge_on = Array.from(document.getElementsByClassName("guardian_huge on"));
+var guardian_huge_i = guardian_huge_on[guardian_huge_on.length-1];
 
-// })
+
+function guardian_checked(checked){
+  var arr1 = Array.from(document.getElementsByClassName("guardian_huge on"));
+  var i1 = arr1[arr1.length-1];
+  var x1 = guardian_huge_on[arr1.length];
+  if(checked.checked==true){
+    i1.classList.remove('on')
+  } else {
+    x1.classList.add('on')
+  }
+};
+
+// var quest_chk1 = document.getElementById('quest_check_box1');
+// var quest_chk2 = document.getElementById('quest_check_box2');
+// var quest_chk2 = document.getElementById('quest_check_box2');
+
+
+var quest_huge = Array.from(document.getElementsByClassName('quest_huge'));
+var quest_huge_on = Array.from(document.getElementsByClassName("quest_huge on"));
+var quest_huge_i = quest_huge_on[quest_huge_on.length-1];
+
+
+function quest_checked(checked){
+  var arr2 = Array.from(document.getElementsByClassName("quest_huge on"));
+  var i2 = arr2[arr2.length-1];
+  var x2 = quest_huge_on[arr2.length];
+  if(checked.checked==true){
+    i2.classList.remove('on')
+  } else {
+    x2.classList.add('on')
+  }
+};
+
 
